4b0e6fa8c0f124d2af520e2e07ade21a514b81b9
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-10-24 01:21:28 +0100

    feat(contact): add contact form section

--- src/app/(pages)/contato/page.tsx
+++ src/app/(pages)/contato/page.tsx
@@ -1,9 +1,11 @@
 import { ContactHeroSection } from "@/components/sections/ContactHeroSection";
+import { ContactFormSection } from "@/components/sections/ContactFormSection";
 
 export default function ContactPage() {
   return (
     <>
       <ContactHeroSection />
+      <ContactFormSection />
     </>
   );
 }
